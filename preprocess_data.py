b024d9ff7105480b615d745d1ee734b5db0b65ab
Deepak Singh Rautela <114864421+deepak-156@users.noreply.github.com>
2023-10-06 12:35:49 +0530

    preprocess_data.py

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-# Import sklearn.preprocessing.StandardScaler
 from sklearn.preprocessing import MinMaxScaler
 
 
@@ -10,7 +9,7 @@ def get_normalised_data(data):
     :param data: a DataFrame with columns as  ['index','Open','Close','Volume']
     :return: a DataFrame with normalised value for all the columns except index
     """
-    # Initialize a scaler, then apply it to the features
+
     scaler = MinMaxScaler()
     numerical = ['Open', 'Close', 'Volume']
     data[numerical] = scaler.fit_transform(data[numerical])
@@ -24,13 +23,13 @@ def remove_data(data):
     :param data: a record of all the stock prices with columns as  ['Date','Open','High','Low','Close','Volume']
     :return: a DataFrame with columns as  ['index','Open','Close','Volume']
     """
-    # Define columns of data to keep from historical stock data
+   
     item = []
     open = []
     close = []
     volume = []
 
-    # Loop through the stock data objects backwards and store factors we want to keep
+ 
     i_counter = 0
     for i in range(len(data) - 1, -1, -1):
         item.append(i_counter)
@@ -39,7 +38,6 @@ def remove_data(data):
         volume.append(data['Volume'][i])
         i_counter += 1
 
-    # Create a data frame for stock data
     stocks = pd.DataFrame()
 
     # Add factors to data frame
@@ -47,7 +45,6 @@ def remove_data(data):
     stocks['Open'] = open
     stocks['Close'] = pd.to_numeric(close)
     stocks['Volume'] = pd.to_numeric(volume)
-
-    # return new formatted data
+    
     return stocks
 
